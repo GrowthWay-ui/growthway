bb4d484cb5a5e8642770c71317f902bba6b1fbce
GrowthWay-ui <sitesathi24x7@gmail.com>
2026-08-22 14:12:12 +0530

    Add Firebase config

---
+++ firebase-config.js
@@ -0,0 +1,9 @@
+export const firebaseConfig = {
+  apiKey: "AIzaSyCNM9HmhvBbhTPuLPX49Q0_kfFNBxWb_OM",
+  authDomain: "growthway777.firebaseapp.com",
+  projectId: "growthway777",
+  storageBucket: "growthway777.firebasestorage.app",
+  messagingSenderId: "576377172410",
+  appId: "1:576377172410:web:10e1fc173af79f33596d83",
+  measurementId: "G-FKBB9X9D5T"
+};
